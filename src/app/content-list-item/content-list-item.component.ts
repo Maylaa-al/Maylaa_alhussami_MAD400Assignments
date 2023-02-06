0bde8aeb9cc7d5253117be0fb175ed62677dfc3a
Maylaa-al <123242347+Maylaa-al@users.noreply.github.com>
2023-02-06 06:46:37 -0500

    Steps 8, 9, 10

--- src/app/content-list-item/content-list-item.component.ts
+++ src/app/content-list-item/content-list-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IContent } from '../icontent';
 
 @Component({
   selector: 'app-content-list-item',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./content-list-item.component.scss']
 })
 export class ContentListItemComponent {
-
+@Input() contentItemInputValue?: IContent
 }
