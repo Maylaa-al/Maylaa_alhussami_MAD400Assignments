8ef4c0aec481cb5fd32d2e45673c9683ae8886e1
Maylaa-al <123242347+Maylaa-al@users.noreply.github.com>
2023-02-13 23:53:04 -0500

    Step7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlankComponentComponent } from './blank-component/blank-component.component';
+import { MyLazyModuleComponent } from './my-lazy-module/my-lazy-module.component';
 
 const routes: Routes = [
   // {
@@ -10,7 +12,10 @@ const routes: Routes = [
   // .then((c) => c.ContentModule),
   // },
   // { path: 'my-lazy', loadChildren: () => import('./my-lazy-module/my-lazy-module.module').then(m => m.MyLazyModuleModule) }
+    { path: '', redirectTo: '/my-lazy-module', pathMatch: "full" },
+    { path: ":id", component: BlankComponentComponent },
   ];
+  
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
